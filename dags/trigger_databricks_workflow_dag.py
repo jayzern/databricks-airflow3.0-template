c24b86779d3455e4c1251e443edf3c7087d0d9a8
jayzern <jayzern@hotmail.com>
2025-07-19 17:53:30 -0400

    feat: stop at dag versioning

---
+++ dags/trigger_databricks_workflow_dag.py
@@ -0,0 +1,15 @@
+from airflow.sdk import DAG
+from airflow.providers.databricks.operators.databricks import (
+    DatabricksRunNowOperator
+)
+from dags.post_and_users_asset import posts_asset, users_asset
+
+with DAG(
+    dag_id="trigger_databricks_workflow_dag",
+    schedule=(posts_asset & users_asset)
+):
+    run_databricks_workflows = DatabricksRunNowOperator(
+        task_id='trigger_databricks_workflow',
+        databricks_conn_id="databricks_conn",
+        job_id="879157959924010"
+    )
